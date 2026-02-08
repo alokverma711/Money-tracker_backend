7d3736aa5ef27c8103811b47fcc1823d0637ba6d
alokverma711 <AlokVerma005@hotmail.com>
2026-02-08 21:42:28 +0530

    Handle OPTIONS and echo Origin in CORS middleware

--- tracker/middleware.py
+++ tracker/middleware.py
@@ -1,11 +1,22 @@
 
+from django.http import HttpResponse
+
 class ForceCorsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        response = self.get_response(request)
-        response["Access-Control-Allow-Origin"] = "*"
+        origin = request.headers.get("Origin")
+        allow_origin = origin if origin else "*"
+
+        if request.method == "OPTIONS":
+            response = HttpResponse()
+        else:
+            response = self.get_response(request)
+
+        response["Access-Control-Allow-Origin"] = allow_origin
+        response["Access-Control-Allow-Credentials"] = "true"
         response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization, X-Requested-With, X-CSRFToken"
+        response["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, Origin, X-Requested-With, X-CSRFToken"
+        response["Access-Control-Max-Age"] = "600"
         return response
